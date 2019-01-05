4ea97345dbae4462c74739d9f7a2d35b93e53e6f
JMR-jmr <45375796+JMR-jmr@users.noreply.github.com>
2019-01-05 21:45:06 +0800

    🆒

--- QuickSort.java
+++ QuickSort.java
@@ -1,3 +1,40 @@
 public class QuickSort{
-
+  public static void quickSort(int[] ints, int begain, int end){
+    int start = begain;
+    int last = end;
+    int key = ints[begain]；
+      if(last-start<1){
+        return;
+      }
+      while(last>start){
+        if(last>start&&ints[last]>=key){
+           end--;
+        }
+        else{
+          if(ints[end]<=key){
+            int temp = ints[end];
+            ints[end]= ints[start];
+             ints[start] = temp;
+          }
+        }
+      if(end>start&&ints[start]>=key){
+        start++;
+      }
+      else{
+        if(ints[start]>=key){
+          int temp = ints[start];
+          ints[end] = ints[start];
+          ints[start] = temp;
+        }
+      }
+      
+      
+      
+      }
+      quickSort(ints, begain, start-1);
+      quickSort(ints, end, last-1);
+      
+      
+      }
+  
 }
